815962c00154bf53785dacd08cb717c489fcfb80
Bjoern2 <bjoern2@github.fake>
2013-04-27 23:03:47 +0200

    Main class added.

---
+++ yolo-tyrion/src/main/java/com/github/bjoern2/yolotyrion/YoloTyrion.java
@@ -0,0 +1,39 @@
+package com.github.bjoern2.yolotyrion;
+
+import com.github.bjoern2.yolotyrion.interfaces.PropertyRepository;
+import com.github.bjoern2.yolotyrion.interfaces.TemplateRepository;
+
+public class YoloTyrion {
+
+	private static TemplateRepositoryProxyFactory templateRepositoryProxyFactory;
+	private static PropertyRepositoryProxyFactory propertyRepositoryProxyFactory;
+	
+	public static <X> X create(Class<X> clazz) {
+		Class<?>[] interfaces = clazz.getInterfaces();
+		for (Class<?> interfaze : interfaces) {
+			if (interfaze.equals(TemplateRepository.class)) {
+				return getTemplateRepositoryProxyFactory().generateProxy(clazz);
+			} else if (interfaze.equals(PropertyRepository.class)) {
+				return getPropertyRepositoryProxyFactory().generateProxy(clazz);
+			}
+		}
+		throw new RuntimeException("Cannot find any interface.");
+		
+		
+	}
+	
+	private static TemplateRepositoryProxyFactory getTemplateRepositoryProxyFactory() {
+		if (templateRepositoryProxyFactory == null) {
+			templateRepositoryProxyFactory = new TemplateRepositoryProxyFactory();
+		}
+		return templateRepositoryProxyFactory;
+	}
+
+	public static PropertyRepositoryProxyFactory getPropertyRepositoryProxyFactory() {
+		if (propertyRepositoryProxyFactory == null) {
+			propertyRepositoryProxyFactory = new PropertyRepositoryProxyFactory();
+		}
+		return propertyRepositoryProxyFactory;
+	}
+	
+}
